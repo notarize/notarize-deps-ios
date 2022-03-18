b8bfba2b5651874b90344f11f53043a05be2ee2a
Aaron Roggow <aaron.roggow@notarize.com>
2022-03-18 11:17:05 -0400

    [SE-1056] added TwilioConversationsClient

--- Package.swift
+++ Package.swift
@@ -38,6 +38,9 @@ let package = Package(
         .package(name: "TwilioVideo",
                  url: "https://github.com/twilio/twilio-video-ios",
                  .exact("4.6.0"))
+        .package(name: "TwilioConversationsClient",
+                 url: "https://github.com/twilio/conversations-ios",
+                 .exact("2.2.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -57,6 +60,7 @@ let package = Package(
                     "Starscream",
                     "Swinject",
                     .product(name: "TwilioVideo", package: "TwilioVideo"),
+                    .product(name: "TwilioConversationsClient", package: "TwilioConversationsClient")
                 ]),
         .binaryTarget(name: "NotarizeCV",
                       url: "https://ios-notarize-signer-sdk.s3.us-west-1.amazonaws.com/NotarizeCV-2.8.0.xcframework.zip",
