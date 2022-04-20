00735d462a5fc52a57d707e77d19e0915952849c
Aaron Roggow <aaron.roggow@notarize.com>
2022-04-20 12:58:00 -0400

    moved PSPDFKit to self hosted binary and updated to 11.3.1

--- Package.swift
+++ Package.swift
@@ -16,7 +16,9 @@ let package = Package(
         .library(name: "NotarizeCV",
                          targets: ["NotarizeCV"]),
         .library(name: "opencv2",
-                         targets: ["opencv2"])
+                         targets: ["opencv2"]),
+        .library(name: "PSPDFKit",
+                         targets: ["PSPDFKit", "PSPDFKitUI"]),
     ],
     dependencies: [
         .package(name: "Apollo", url: "https://github.com/apollographql/apollo-ios", .exact("0.48.0")),
@@ -25,7 +27,6 @@ let package = Package(
         .package(name: "Kingfisher",
                  url: "https://github.com/onevcat/Kingfisher",
                  .exact("7.1.1")),
-        .package(name: "PSPDFKit", url: "https://github.com/PSPDFKit/PSPDFKit-SP", .exact("11.3.0")),
         .package(name: "Reachability", url: "https://github.com/ashleymills/Reachability.swift", .exact("4.3.1")),
         .package(name: "Segment",
                  url: "https://github.com/segmentio/analytics-ios",
@@ -49,7 +50,6 @@ let package = Package(
                     .product(name: "ApolloUtils", package: "Apollo"),
                     .product(name: "JWTDecode", package: "JWTDecode"),
                     .product(name: "Kingfisher", package: "Kingfisher"),
-                    .product(name: "PSPDFKit", package: "PSPDFKit"),
                     .product(name: "Reachability", package: "Reachability"),
                     .product(name: "Segment", package: "Segment"),
                     .product(name: "Sentry", package: "Sentry"),
@@ -63,6 +63,12 @@ let package = Package(
                       checksum: "1cce6279d4ad5e348bd77ab96b0a780ced4a7ecb806a6adee70494585ed2455a"),
         .binaryTarget(name: "opencv2",
                       url: "https://ios-notarize-signer-sdk.s3.us-west-1.amazonaws.com/opencv2-4.5.4.xcframework.zip",
-                      checksum: "4dc73197aec00c85667a18bb02c20b476ee1e6b16705cc28f56ce7a22d44446e")
+                      checksum: "4dc73197aec00c85667a18bb02c20b476ee1e6b16705cc28f56ce7a22d44446e"),
+        .binaryTarget(name: "PSPDFKit",
+                      url: "https://ios-notarize-signer-sdk.s3.us-west-1.amazonaws.com/PSPDFKit-for-iOS-PSPDFKit.xcframework-11.3.1.zip",
+                      checksum: "8b72c61db0497111cb5e94af2269c8e8a5a7b5935f5600d87264e1df266f0144"),
+        .binaryTarget(name: "PSPDFKitUI",
+                      url: "https://ios-notarize-signer-sdk.s3.us-west-1.amazonaws.com/PSPDFKit-for-iOS-PSPDFKitUI.xcframework-11.3.1.zip",
+                      checksum: "d2ba1b1a01ac5a4cb36610200f8e13e671d8608608a6dcde46721e9b0ab04ab1"),
     ]
 )
